f51cda3bd56d5ee7fe74d7700b0ec1b9e9ca7c7c
saiful110816 <saiful110816@acc.edu.bd>
2024-10-02 20:25:15 +0600

    sdfsdf

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2025, 0, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2024, 12, 14, 12, 59, 59)}); // year, month, date, hour
         });
 });		
 
